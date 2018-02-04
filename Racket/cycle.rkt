a314dc36b3be2269e64f51c8f453f53b22f617b1
¨Martin <repickym@gmail.com>
2018-02-04 12:21:01 +0000

    lcycle working

--- Racket/cycle.rkt
+++ Racket/cycle.rkt
@@ -1,10 +1,27 @@
 #lang racket
 
-(define (rcycle-helper m n)
+; Hepler for lcycle-old function. Creates list within a list ((2 3 4 5) 1)
+(define (lcycle-helper m n)
   (if (pair? m)
-      (append m (rcycle-helper (car m) n))
-       (list n)))
+      (cons m (lcycle-helper (car m) n))
+      (list n)))
 
-(define (rcycle m)
-  (rcycle-helper (cdr m) (car m)))
+; Left cycle the list. Now working properly. List within the list.
+(define (lcycle-old m)
+  (lcycle-helper (cdr m) (car m)))
 
+; Very simple version using append. Working correctly
+(define (lcycle-append m)
+  (append (cdr m) (list (car m))))
+
+; Helper funtion for lcycle takes in n:first number of array m:rest of the array
+; Takes in example m:(2 3 4 5) n:1 returns (2 3 4 5 1)
+(define (lcycleH m n)
+  (if(pair? m)
+     (cons (car m) (lcycleH (cdr m) n))
+     (list n)))
+
+; Properly working lcycle function.
+; Example: m:(1 2 3 4 5) returns (2 3 4 5 6)
+(define (lcycle m)
+  (lcycleH (cdr m) (car m)))
