88ed9b9a3077fd3b4e60e084cb5e6f270ffd2db8
¨Martin <repickym@gmail.com>
2018-02-04 15:55:46 +0000

    comments

--- Racket/cycle.rkt
+++ Racket/cycle.rkt
@@ -1,33 +1,45 @@
 #lang racket
 
-; Hepler for lcycle-old function. Creates list within a list ((2 3 4 5) 1)
+; Helper for lcycle-old function.
+; Not working correctyl. Creates list within a list.
+; Example: (lcycle-helper '(2 3 4 5) 1)
+; Returns: '((2 3 4 5) 1)
 (define (lcycle-helper m n)
   (if (pair? m)
       (cons m (lcycle-helper (car m) n))
-      (list n)))
+      (list (car n))))
 
-; Left cycle the list. Now working properly. List within the list.
+; Cyclically shift the list to the left.
+; Not working properly. Creates a List within the list.
+; Example: (lcycle-old (list 1 2 3 4 5))
+; Returns: '((2 3 4 5) 1)
 (define (lcycle-old m)
-  (lcycle-helper (cdr m) (car m)))
+  (lcycle-helper (cdr m) m))
 
-; Very simple version using append. Working correctly
+; Very simple version using append. Works correctly.
+; Example: (lcycle-append (list 1 2 3 4 5))
+; Returns: '(2 3 4 5 1)
 (define (lcycle-append m)
   (append (cdr m) (list (car m))))
 
-; Helper funtion for lcycle takes in n:first number of array m:rest of the array
-; Takes in example m:(2 3 4 5) n:1 returns (2 3 4 5 1)
+; Helper funtion for lcycle. n:first number of array m:rest of the array
+; Example m:(2 3 4 5) n:1
+; Returns '(2 3 4 5 1)
 (define (lcycleH m n)
   (if(pair? m)
      (cons (car m) (lcycleH (cdr m) n))
      (list n)))
 
 ; Properly working lcycle function.
-; Example: m:(1 2 3 4 5) returns (2 3 4 5 6)
+; Example: m:(1 2 3 4 5)
+; Returns '(2 3 4 5 6)
 (define (lcycle m)
   (lcycleH (cdr m) (car m)))
 
-; First helping function for rcycle.
-; Returns last element of the list
+; Helping function for rcycle.
+; Returns last element of the list.
+; Example:  (lastE (list 1 2 3 4 5))
+; Returns: 5
 (define (lastE m)
   (if(pair? (cdr m))
      (lastE (cdr m))
@@ -35,6 +47,8 @@
 
 ; Helping function for rcycle.
 ; Returns list m without last Element of the list.
+; Example: (list-no-last (list 1 2 3 4 5))
+; Returns: '(1 2 3 4)
 (define (list-no-last m)
 (if(pair? (cdr (cdr m)))
    (cons (car m)
@@ -42,9 +56,8 @@
    (list (car m))))
 
 ; rcycle function. Shifts the list cyclically, one place to the right.
-; Example: (rcycle (list 1 2 3 4 5)) returns: '(5 1 2 3 4)
+; Example: (rcycle (list 1 2 3 4 5))
+; Returns: '(5 1 2 3 4)
 (define (rcycle m)
   (if (pair? m) (cons (lastE m) (list-no-last m))
       m))
-
-
