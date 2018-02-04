84ecbff2c7f6a37987ad9608f7447928b8948681
¨Martin <repickym@gmail.com>
2018-02-04 16:00:25 +0000

    typos

--- Racket/cycle.rkt
+++ Racket/cycle.rkt
@@ -11,13 +11,13 @@
 
 ; Cyclically shift the list to the left.
 ; Not working properly. Creates a List within the list.
-; Example: (lcycle-old (list 1 2 3 4 5))
+; Example: (lcycle-old '(1 2 3 4 5))
 ; Returns: '((2 3 4 5) 1)
 (define (lcycle-old m)
   (lcycle-helper (cdr m) m))
 
 ; Very simple version using append. Works correctly.
-; Example: (lcycle-append (list 1 2 3 4 5))
+; Example: (lcycle-append '(1 2 3 4 5))
 ; Returns: '(2 3 4 5 1)
 (define (lcycle-append m)
   (append (cdr m) (list (car m))))
@@ -32,13 +32,13 @@
 
 ; Properly working lcycle function.
 ; Example: m:(1 2 3 4 5)
-; Returns '(2 3 4 5 6)
+; Returns '(2 3 4 5 1)
 (define (lcycle m)
   (lcycleH (cdr m) (car m)))
 
 ; Helping function for rcycle.
 ; Returns last element of the list.
-; Example:  (lastE (list 1 2 3 4 5))
+; Example:  (lastE '(1 2 3 4 5))
 ; Returns: 5
 (define (lastE m)
   (if(pair? (cdr m))
@@ -47,7 +47,7 @@
 
 ; Helping function for rcycle.
 ; Returns list m without last Element of the list.
-; Example: (list-no-last (list 1 2 3 4 5))
+; Example: (list-no-last '(1 2 3 4 5))
 ; Returns: '(1 2 3 4)
 (define (list-no-last m)
 (if(pair? (cdr (cdr m)))
@@ -56,7 +56,7 @@
    (list (car m))))
 
 ; rcycle function. Shifts the list cyclically, one place to the right.
-; Example: (rcycle (list 1 2 3 4 5))
+; Example: (rcycle '(1 2 3 4 5))
 ; Returns: '(5 1 2 3 4)
 (define (rcycle m)
   (if (pair? m) (cons (lastE m) (list-no-last m))
