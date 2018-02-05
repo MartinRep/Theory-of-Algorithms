2f02ea7847adf041003c1f99f7fdf05b5adad756
¨Martin <repickym@gmail.com>
2018-02-05 11:02:27 +0000

    Comments tidy up

--- Racket/collatz.rkt
+++ Racket/collatz.rkt
@@ -1,7 +1,18 @@
+; Adapted from https://en.wikipedia.org/wiki/Collatz_conjecture
+
+; The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined as follows:
+; start with any positive integer n. Then each term is obtained from the previous term as follows:
+; if the previous term is even, the next term is one half the previous term.
+; Otherwise, the next term is 3 times the previous term plus 1. The conjecture is that no matter what value of n,
+; the sequence will always reach 1.
+
+
 #lang racket
 
 ; Helper function. If m is a odd number multiplies it by 3 and add 1
 ; If m is even number divides it by 2
+; Example: (collatz-helper 7) Returns: 22
+; Example: (collatz-helper 22) Returns: 11
 (define (collatz-helper m)
   (if (= (remainder m 2) 0)
   (/ m 2)
@@ -11,6 +22,8 @@
 ; Uses collatz-helper to determine next sequence number and assain it as n
 ; If n = 1 creates a list
 ; Else calls itself with number n and adds the result to created list
+; Example: > (collatz-list-old 10)
+; Returns: '(10 5 16 8 4 2 1)
 (define (collatz-list-old m)
   (let ([n (collatz-helper m)])
         (if (= n 1)
@@ -18,6 +31,8 @@
             (cons m (collatz-list-old n )))))
 
 ; Simplified version
+; Example: > (collatz-list-old 10)
+; Returns: '(10 5 16 8 4 2 1)
 (define (collatz-list m)
   (if (= m 1)
       (list m)
--- Racket/prime.rkt
+++ Racket/prime.rkt
@@ -1,26 +1,46 @@
 ; Adapted from https://docs.racket-lang.org/reference/pairs.html
+; http://mathforum.org/library/drmath/view/57036.html
+
+; A prime number is one with exactly two positive divisors, itself and one.
+; 1 is neither a prime nor a composite number. Zero is not a prime or a composite number either. Zero has an infinite 
+; number of divisors.
+; By the usual definition of prime for integers, negative integers can not be prime.
+; Negative integers can be prime. In fact the integer -p is prime whenever p,
+; but since they are associates, we really do not have any new primes.
+; So in this example we only cosider number n > 1.
 #lang racket
 
 ; andmap return first #f from the lambda function.
 ; lamda return #f if remainder of n and div is 0. n is not prime number.
 ; List of dividers to feed the lambda function. Numbers from 2 until n/2. +1 had to be added for the case of '4'.
-(define (prime? n)
-  (andmap (lambda (div) (if (= (remainder n div) 0) #f #t))
-          (stream->list (in-range 2 (+ (/ n 2) 1)))))
-
+; in-range creates stream of integers.
+; stream->list converts stream from into list, which feeds andmap
+; Example: (prime? 7)
+; Returns: #t
+(define (isprime? n)
+  (if (> n 1)
+      (andmap (lambda (div) (if (= (remainder n div) 0) #f #t))
+          (stream->list (in-range 2 (+ (/ n 2) 1))))
+      #f))
 
 ; Helper function to calculate if the number is prime using recursion.
 ; Inputs m:Number to check n:Number to divide m to check if there is a remainer.
 ; Every recursion adds 1 to n.
 ; Keep recuring until n is m/2.
+; Example: (prime-helper 12 2)
+; Returns: #f
 (define (prime-helper m n)
-  (if (> n (/ m 2)) #t
-   (if (= (remainder m n) 0)
-       #f
-       (prime-helper m (+ n 1))
-       )))
+  (if (> n (/ m 2))
+      #t ;end of the recursion. No dividers left
+      (if (= (remainder m n) 0)
+          #f ; Divided with 0 remainder. Divisor found.
+          (prime-helper m (+ n 1)))))
 
 ; Recursive version with helper method. Using method prime-helper to calculate prime for each posible divisions.
-; Provide smallest divider as second argument 2
-(define (isprime? m)
-      (prime-helper m 2))
+; Check for positive number bigger than 1 and provide smallest divider for prime-helper.
+; Example: (define (isprime? 7)
+; Returns: #t
+(define (prime? m)
+  (if (> m 1)
+      (prime-helper m 2)
+      #f)) ; (0 1) are not prime numbers. 
