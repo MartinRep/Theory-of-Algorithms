2cf30422b24d306e9ed1eaebc7b907ab10e097d0
repickym <repickym@gmail.com>
2018-04-07 20:11:17 +0100

    sublsum finished and commented

--- Racket/sublsum.rkt
+++ Racket/sublsum.rkt
@@ -1,38 +1,28 @@
 #lang racket
 
+;Sums elements of the list L, if empty returns 0
 (define (mySum L)
-  (if (empty? L) 0
+  (if (null? L) 0
       (+ (car L) (mySum (cdr L))))
   )
 
-(define (myMap f l)
-  (if (null? l)
+; My implementation of map function. Applies all the elements of list l to the function f.
+(define (myMap F L)
+  (if (null? L)
       null
-      (cons (f (car l)) (myMap f (cdr l)))))
+      (cons (F (car L)) (myMap F (cdr L)))))
 
 
-
+; Sums all the elements of the list and return list of the all the possible combinations of list elements
+; that sums to 0.(combination l) returns list of all the possible combinations of elements, lambda function applies it to the if statement
+; which calls mySum function to check if summary of elements is 0. The results are filtered out through filter function with condition pair?,
+; this eliminates all the empty list.
+; > (sublsum '(1 2 3 5 4 -5))
+; '((2 3 -5) (5 -5) (1 4 -5))
 (define (sublsum l)
-  (myMap (lambda(subl) (if (= (mySum subl) 0)
-                         subl
-                         #t))
-       (combinations l)))
+  (filter pair?(myMap (lambda(subl) (if (= (mySum subl) 0)
+                                        subl
+                                        null))
+                      (combinations l))))
 
-(define (subLsum subL)
-  (if (null? subL)
-      null
-      (cons (if (= (mySum (car subL)) 0)
-                (cons (car subL) (subLsum (cdr subL)))
-                (subLsum (cdr subL)))
-            (subLsum (cdr subL)))
-  )
-)
 
-(define (subLsumStart l)
-  (subLsum (combinations l)))
-
-(define (cleanL l)
-  (if (pair? l)
-      (cons (car l) (cleanL (cdr l)))
-      '()
-      ))
